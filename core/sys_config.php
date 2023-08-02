519dc7a4091d6206565e662c922c9c5bd09c04bb
Roger0899 <66247356+Roger0899@users.noreply.github.com>
2023-08-02 01:14:44 -0500

    conf2

--- core/sys_config.php
+++ core/sys_config.php
@@ -2,7 +2,7 @@
 if (! defined ( 'SRCP' )) {
 	die ( "Logged Hacking attempt!" );
 }
-// TODO: Hacer algo util con esto.
+// TODO: Hacer algo util con esto
 $config = array (
 'version_id' => "1.0",
 'reg_prof' => "",
